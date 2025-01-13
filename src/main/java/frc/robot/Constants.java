7f6b2e6810ba9bda9ac0bf5bff198153ebee0b42
lschroeder07 <lschroeder@columbusschoolforgirls.org>
2025-01-13 18:52:18 -0500

    Added constants folder and implemented wheel constants into swerve drive

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+public class Constants {
+
+    //swerve constants
+    public static final double WHEEL_RADIUS = 0.0508; //TODO: change value 
+    public static final int ENCODER_RES = 4096; //TODO: change value
+
+}
