665ace4f0d5a3a2f43f6181b77b92d33342c8873
Team 677 <firstteam677@columbusschoolforgirls.org>
2025-01-22 20:29:02 -0500

    Added 2024 limelight code for base

---
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -0,0 +1,52 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class Limelight {
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTableEntry tx = table.getEntry("tx"); // x axis position
+    NetworkTableEntry ty = table.getEntry("ty"); // y axis position
+    NetworkTableEntry tv = table.getEntry("tv"); // is there valid target
+    NetworkTableEntry ta = table.getEntry("ta"); // area in view
+    NetworkTableEntry ts = table.getEntry("ts0"); // area in view
+    NetworkTableEntry pos = table.getEntry("camera-pose_targetspace"); // 3D translation and rotations?
+    NetworkTableEntry pos1 = table.getEntry("target-pose_cameraspace");
+    NetworkTableEntry pos2 = table.getEntry("target-pose_robotspace");
+
+    Drivetrain driveTrain;
+    double ySpeed = 0;
+    double rotationSpeed = 0;
+    long lastTargetTime = 0;
+
+    public Limelight(Drivetrain drive) {
+        this.driveTrain = drive;
+
+    }
+
+    public void updateLimelight() {
+
+    }
+
+    public double getRotation() {
+        return pos.getDoubleArray(new double[6])[5]; // get rotation z value from botpose array
+
+    }
+
+    public double getX() {
+        return tx.getDouble(0);
+    }
+
+    public double getY() {
+        return ty.getDouble(0);
+
+    }
+
+    public double getA() {
+        return ta.getDouble(0);
+
+    }
+}
+
+
