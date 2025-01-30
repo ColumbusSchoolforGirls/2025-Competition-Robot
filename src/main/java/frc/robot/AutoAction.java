ee25e8d3812497f12e7748cc6193271b2eecd9b6
Team 677 <firstteam677@columbusschoolforgirls.org>
2025-01-29 19:41:59 -0500

    Auto code outline

---
+++ src/main/java/frc/robot/AutoAction.java
@@ -0,0 +1,6 @@
+package frc.robot;
+
+public enum AutoAction {
+    DRIVE, TURN, SHOOT_CORAL, ELEVATOR, REMOVE_ALGAE //implement aligning into drive
+
+}
---
+++ src/main/java/frc/robot/AutoPaths.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+public class AutoPaths { //obviously need to add more
+
+    public static AutoStep[] autoLeftMain = {
+
+
+    };  
+    
+    public static AutoStep[] autoMiddleMain = {
+
+
+    };  
+
+    public static AutoStep[] autoRightMain = {
+
+
+    };  
+}
